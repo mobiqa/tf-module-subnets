1326ae090366fd7ff89b03b4d8b5ef6546773e22
venkat <venkat.mobiqa@gmail.com>
2023-03-05 15:35:29 +0530

    updating ./tf-module-subnets

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "vpc_peering_connection_id" {
-  value = aws_vpc_peering_connection.peer.id
-}
-
-output "internet_gw_id" {
-  value = aws_internet_gateway.igw.id
+output "subnet_ids" {
+  value = aws_subnet.main.*.id
 }
